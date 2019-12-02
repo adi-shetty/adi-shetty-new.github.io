472cdffcb08a8abba44c4f786cb4aa452f4a2bb8
adityashetty <adishetty1234@gmail.com>
2019-12-02 12:42:25 +0530

    add all

---
+++ precache-manifest.e5041bada062059b685cb53de1167627.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7cb26e8c00558812340c30061be0b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "343714e28abad02cbffa",
+    "url": "/static/css/2.092d3ae3.chunk.css"
+  },
+  {
+    "revision": "043646f0b074fcf4e0d3",
+    "url": "/static/css/main.edf6166e.chunk.css"
+  },
+  {
+    "revision": "343714e28abad02cbffa",
+    "url": "/static/js/2.a2adec4b.chunk.js"
+  },
+  {
+    "revision": "043646f0b074fcf4e0d3",
+    "url": "/static/js/main.40274e18.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "5223c19ac8f1930bacb58bcf8e20aec2",
+    "url": "/static/media/SampleData.5223c19a.png"
+  },
+  {
+    "revision": "c645a83d13e5d5085f16cbb7a0c6281a",
+    "url": "/static/media/bl.c645a83d.png"
+  },
+  {
+    "revision": "77019cf6262adf65cebb340f5e258ce8",
+    "url": "/static/media/cancel.77019cf6.png"
+  },
+  {
+    "revision": "9fbd361275e527c242913ac93f0bf505",
+    "url": "/static/media/cf.9fbd3612.png"
+  },
+  {
+    "revision": "f6eacff2efe88184c023801824862cc4",
+    "url": "/static/media/edit.f6eacff2.png"
+  },
+  {
+    "revision": "4cb2269f02c805ec2cdfb239e8f14099",
+    "url": "/static/media/gauge.4cb2269f.png"
+  },
+  {
+    "revision": "7870e02f628185c9fe9523871763261f",
+    "url": "/static/media/pl.7870e02f.png"
+  },
+  {
+    "revision": "9c0148c6ba2b8cc1ca4885f57e46efcc",
+    "url": "/static/media/ratios.9c0148c6.png"
+  },
+  {
+    "revision": "c5b47b8c4f461b4bddf5c5ce2984b4dc",
+    "url": "/static/media/stocks_wallpaper.c5b47b8c.jpg"
+  },
+  {
+    "revision": "d33f7f6b6cd30cde0b0ce4e41c8cdbbd",
+    "url": "/static/media/stockslogo.d33f7f6b.PNG"
+  },
+  {
+    "revision": "7ac9ba43153e1ef94751fbbbb9e8162c",
+    "url": "/static/media/wallet.7ac9ba43.png"
+  }
+]);
